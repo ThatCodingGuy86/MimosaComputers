c0fce9195cefa31357c4ef3255f5a8ba7f400880
ThatCodingGuy86 <75764677+ThatCodingGuy86@users.noreply.github.com>
2022-12-01 21:31:33 -0600

    first commit

---
+++ src/main/java/bluefire_06/mimosa_computers/MimosaComputers.java
@@ -0,0 +1,15 @@
+package bluefire_06.mimosa_computers;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MimosaComputers implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger("mimosa_computers");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing...");
+    }
+}
---
+++ src/main/java/bluefire_06/mimosa_computers/client/MimosaComputersClient.java
@@ -0,0 +1,11 @@
+package bluefire_06.mimosa_computers.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class MimosaComputersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
